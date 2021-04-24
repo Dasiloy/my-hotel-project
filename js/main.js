209764fa55615d9af7a4399e8cc2cf137d30f9a9
Damilare50 <66230820+Damilare50@users.noreply.github.com>
2021-04-25 00:31:39 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ $(document).ready(
        $('#navbar').toggleClass('scroll',$(this).scrollTop() > 90); 
       }
     );
-
-
   }
 );
+
+function validateSubscriberEmail() {
+  var em = document.forms["emailSubs"]["subsEmail"].value;
+  if (em == "") {
+    alert("Email field must be filled out!");
+    return false;
+  } else {
+    alert("Subscribed successfully!");
+  };
+}
+
+function submitContactForm() {
+  if (document.forms["contactForm"]["contactFormName"].value == "") {
+	  alert("Name field must be filled out!");
+  }
+
+  if (document.forms["contactForm"]["contactFormEmail"].value == "") {
+      alert("Email field must be filled out!");
+  }
+  
+  if (document.forms["contactForm"]["contactFormBody"].value == "") {
+      alert("Response cannot be empty!");
+  }
+}
